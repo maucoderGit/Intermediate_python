4aa205545098ad565236e15ff08c3f486387de21
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-08 19:08:47 -0400

    using map and filter

--- data_recolector.py
+++ data_recolector.py
@@ -5,12 +5,18 @@ from platzi_workers import DATA
 def run():
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
     all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    adults = list(filter(lambda worker: worker["age"] >= 18 , DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
 
-    for worker in all_python_devs:
+    # for worker in all_python_devs:
+    #     print(worker)
+
+    # for worker in all_Platzi_workers:
+    #     print(worker)
+
+    for worker in adults:
         print(worker)
 
-    for worker in all_Platzi_workers:
-        print(worker)
 
 if __name__ == "__main__":
     run()

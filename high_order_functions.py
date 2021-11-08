3e730afadc573f33f04a4556b5c13424dd8f265e
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-08 17:47:00 -0400

    - We learned high order functions

--- high_order_functions.py
+++ high_order_functions.py
@@ -1,8 +1,23 @@
-def run():
-    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+from functools import reduce
 
-    new_list = filter(lambda number: number % 2 == 0, my_list)
+def run():
+   # Filter 
+    my_list = [1, 2, 3, 4, 5]
+    new_list = list(filter(lambda x: x % 2 == 0, my_list))
+    
     print(new_list)
+
+   # Map
+    squares = list(map(lambda x: x ** 2, my_list))
+
+    print(squares)
+
+   # Reduce
+    exit_list = [2, 2, 2, 2 ,2]
+    other_list = reduce(lambda a, b: a + b, exit_list)
+    
+    print(other_list)
+
 
 if __name__ == "__main__":
     run()

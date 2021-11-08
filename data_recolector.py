2ea04797a24d1542cfc86b627bdcc47aee3b5759
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-08 18:41:03 -0400

    list_comprenhensions

--- data_recolector.py
+++ data_recolector.py
@@ -3,8 +3,14 @@
 from platzi_workers import DATA
 
 def run():
-    pass
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
 
+    for worker in all_python_devs:
+        print(worker)
+
+    for worker in all_Platzi_workers:
+        print(worker)
 
 if __name__ == "__main__":
     run()

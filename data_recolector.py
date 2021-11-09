9846ed73aba8b3c95874c2a3a905dc86063add48
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-09 17:24:39 -0400

    Challenges Completed

--- data_recolector.py
+++ data_recolector.py
@@ -3,19 +3,36 @@
 from platzi_workers import DATA
 
 def run():
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    adults = list(filter(lambda worker: worker["age"] >= 18 , DATA))
-    adults = list(map(lambda worker: worker["name"], adults))
-    old_peaple = list(map(lambda worker: worker | {"old" : worker["age"] > 70}, DATA))
+    # Class Examples
 
+    # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # adults = list(filter(lambda worker: worker["age"] >= 18 , DATA))
+    # adults = list(map(lambda worker: worker["name"], adults))
+    old_people = list(map(lambda worker: worker | {"old" : worker["age"] > 70}, DATA))
+
+    # Challenge
+
+    all_python_devs = list(filter(lambda worker: worker["language"] == "python", DATA))
+    all_python_devs = list(map(lambda worker: worker["name"], all_python_devs))
+    all_python_workers = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+    all_python_workers = list(map(lambda worker: worker["name"], all_python_workers))
+
+    adults = [worker["name"] for worker in DATA if worker["age"] >= 18]
+    old_people = [worker["age"] for worker in DATA if worker["age"] > 70] 
+
+    for worker in adults:
+        print(worker)
+
+    # Class Executions:
+    
     # for worker in all_python_devs:
     #     print(worker)
 
     # for worker in all_Platzi_workers:
     #     print(worker)
 
-    for worker in old_peaple:
+    for worker in old_people:
         print(worker)
 
 

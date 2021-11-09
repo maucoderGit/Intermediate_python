2be16d5b9fb6a40f1570a3c8ba33103a88885556
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-09 00:02:07 -0400

    ending first proyect

--- data_recolector.py
+++ data_recolector.py
@@ -7,6 +7,7 @@ def run():
     all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
     adults = list(filter(lambda worker: worker["age"] >= 18 , DATA))
     adults = list(map(lambda worker: worker["name"], adults))
+    old_peaple = list(map(lambda worker: worker | {"old" : worker["age"] > 70}, DATA))
 
     # for worker in all_python_devs:
     #     print(worker)
@@ -14,7 +15,7 @@ def run():
     # for worker in all_Platzi_workers:
     #     print(worker)
 
-    for worker in adults:
+    for worker in old_peaple:
         print(worker)
 
 

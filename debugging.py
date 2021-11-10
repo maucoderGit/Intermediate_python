1e8f38395cca6fa8ec39ad09b6fe7188abcd1d02
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-09 23:35:30 -0400

    Using exceptions and error manage

--- debugging.py
+++ debugging.py
@@ -1,11 +1,22 @@
 def divisors(num):
-    divisors = [i for i in range(1, num + 1) if num % i == 0]
-    return divisors
+    try: 
+        if num < 0:
+            raise ValueError("Ingresa un número positivo")
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+
+    except ValueError as ve:
+        print(ve)
+
 
 def run():
-    num = int(input("ingresa un número: "))
-    print(divisors(num))
-    print("Termino el programa")
+    try:
+        num = int(input("ingresa un número: "))
+        print(divisors(num) or "No pude ejecutar esto.")
+        print("Termino el programa")
+    
+    except ValueError:
+        print("debes ingresar un número")
 
 
 if __name__ == "__main__":

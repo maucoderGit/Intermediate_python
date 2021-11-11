4b8df54028b10ff2ac30f1e6b785a3c9f10ca9bc
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-11 13:32:45 -0400

    adding files.txt and files folder with numbers.txt

---
+++ files.py
@@ -0,0 +1,24 @@
+from os import WIFCONTINUED
+
+
+def read():
+    numbers = []
+    with open("./files/numbres.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+def write():
+    names = ["Catherina", "Eduard", "Byron", "Dadniellys", "Linda", "Félix"]
+    with open("./files/names.txt", "a", encoding="utf-8") as f:
+        for name in names: 
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    write()
+
+
+if __name__ == "__main__":
+    run()

116f7cf90731811d70024bfe4ed334304839655c
maucoderGit <dreg5000.bbb@gmail.com>
2021-11-22 14:49:44 -0400

    sumary: - function get_word created. - then, we get user word with user_letter input

--- final_proyect/hangman_game.py
+++ final_proyect/hangman_game.py
@@ -1,33 +1,20 @@
-from os import WIFCONTINUED
 from random import randint
 
 
 def read():
-    with open("./files/words.txt", "r", encoding="utf-8") as f:
-        words = [line for line in f]
+    with open("../final_proyect/files/words.txt", "r", encoding="utf-8") as f:
+        words = [line.replace("\n", "") for line in f]
         lenght = int(len(words))
     return words[randint(0, lenght - 1)]
 
     # We rest 1 because last number will be an error
 
 
-def hangman(letter, word):
-    lifes = 7
-    winner = False
-    try:
-        while lifes > 0 or winner == False:
-            if len(letter) != 1:
-                raise ValueError("Debes ingresar solo un caracter.")
-            pass
-
-
-    except ValueError:
-        pass
-
-
 def run():
+    word = read()
+    
     try:
-        word = read()
+        print(word)
         print("Bienvenido a el juego del ahorcado\n")
         user_letter = str(input("Escribe una letra: "))
 
